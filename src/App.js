f2ef996e4ed1e1ceeb97596578f7a44acc786a07
HipoTofu <jlsreyes.student@ua.edu.ph>
2023-09-26 16:29:49 +0800

    wow

--- src/App.js
+++ src/App.js
@@ -9,43 +9,43 @@ function AlertButton({ message, children }) {
 function App() {
   return (
     <section>
+
+    <calcContainer>
+      <displayContainer>
       <h1>
         Welcome to my App
       </h1>
-    <calcContainer>
-      <displayContainer>
         <display>
           0
         </display>
       </displayContainer>
       <row1>
-          <numbers><AlertButton message="7">7</AlertButton></numbers>
-          <numbers><AlertButton message="8">8</AlertButton></numbers>
-          <numbers><AlertButton message="9">9</AlertButton></numbers>
-          <numbers><AlertButton message="+">+</AlertButton></numbers>
+        <AlertButton message="7">7</AlertButton>
+        <AlertButton message="8">8</AlertButton>
+        <AlertButton message="9">9</AlertButton>
+        <AlertButton message="+">+</AlertButton>
+      </row1>
 
-        </row1>
+      <row2>
+        <AlertButton message="4">4</AlertButton>
+        <AlertButton message="5">5</AlertButton>
+        <AlertButton message="6">6</AlertButton>
+        <AlertButton message="-">-</AlertButton>
+      </row2>
 
-        <row2>
-        <numbers><AlertButton message="4">4</AlertButton></numbers>
-        <numbers><AlertButton message="5">5</AlertButton></numbers>
-        <numbers><AlertButton message="6">6</AlertButton></numbers>
-        <numbers><AlertButton message="-">-</AlertButton></numbers>
-        </row2>
+      <row3>
+        <AlertButton message="1">1</AlertButton>
+        <AlertButton message="2">2</AlertButton>
+        <AlertButton message="3">3</AlertButton>
+        <AlertButton message="*">*</AlertButton>
+      </row3>
 
-        <row3>
-        <numbers><AlertButton message="1">1</AlertButton></numbers>
-        <numbers><AlertButton message="2">2</AlertButton></numbers>
-        <numbers><AlertButton message="3">3</AlertButton></numbers>
-        <numbers><AlertButton message="*">*</AlertButton></numbers>
-        </row3>
-
-        <row4>
-        <numbers><AlertButton message="C">C</AlertButton></numbers>
-        <numbers><AlertButton message="0">0</AlertButton></numbers>
-        <numbers><AlertButton message="=">=</AlertButton></numbers>
-        <numbers><AlertButton message="÷">÷</AlertButton></numbers>
-        </row4>
+      <row4>
+        <AlertButton message="C">C</AlertButton>
+        <AlertButton message="0">0</AlertButton>
+        <AlertButton message="=">=</AlertButton>
+        <AlertButton message="÷">÷</AlertButton>
+      </row4>
 
     </calcContainer>
 
